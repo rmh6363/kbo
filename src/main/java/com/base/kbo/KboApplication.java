4e29a95b2acd4230e4618bbef4c8f0c6b07f368b
rmh <rmh63@naver.com>
2024-08-12 11:58:05 +0900

    첫 커밋 메시지

---
+++ src/main/java/com/base/kbo/KboApplication.java
@@ -0,0 +1,13 @@
+package com.base.kbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KboApplication.class, args);
+	}
+
+}
